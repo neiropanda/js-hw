2890e8a2584d0ea696546928e7bfd15f74fa8689
Oleksandr <bekh.fa@gmail.com>
2020-03-09 23:34:37 +0200

    DZ

--- js/app.js
+++ js/app.js
@@ -198,3 +198,236 @@ const car = {
 //     return `car was created in ${this.year}`;
 //   }
 // });
+
+// const array = [1, 2, 3, 4, 5];
+
+// Array.prototype.multBy = function (n) {
+//   return this.map(function (i) {
+//     return i * n;
+//   });
+// }
+
+// console.log(array.multBy(50));
+
+// Функции высшего порядка
+
+function firstFunc(arr, handler) {
+  let res = 'New Value: ';
+  for (let i = 0; i < arr.length; i++) {
+    res += handler(arr[i]);
+  }
+  return res;
+}
+
+const res1 = firstFunc(['my', 'name', 'is', 'sasha'], function (item) {
+  return item[0].toUpperCase() + item.slice(1) + ' ';
+});
+
+const res2 = firstFunc([10, 20, 30], function (item) {
+  return item * 10 + ' ';
+})
+
+// console.log(res1);
+// console.log(res2);
+
+
+function every(arr, handler) {
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!handler(arr[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const isNum = every([2, 3, 4, 5], function (num) {
+  return typeof num === 'number';
+});
+
+// console.log(isNum);
+
+let prod1 = {
+  price: 20,
+  count: 100,
+  getTotalPrice() {
+    return this.price * this.count;
+  }
+};
+
+console.log(prod1.getTotalPrice());
+
+let prod2 = {
+  price: 10,
+  count: 50,
+};
+
+prod1.getTotalPrice.call(prod2);
+
+
+let element = {
+  height: 25,
+  getHeight: function () {
+    return this.height;
+  }
+};
+
+let getElementHeight = element.getHeight.bind(element);
+getElementHeight();
+
+// Переберающие методы массивов
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const newArr = arr.map((num) => ({
+  digit: num,
+  odd: Boolean(num % 2),
+}));
+
+// console.log(newArr);
+
+const stringArr = [{
+    char: "a",
+    index: 12
+  }, {
+    char: "w",
+    index: 8
+  }, {
+    char: "Y",
+    index: 10
+  }, {
+    char: "p",
+    index: 3
+  }, {
+    char: "p",
+    index: 2
+  },
+  {
+    char: "N",
+    index: 6
+  }, {
+    char: " ",
+    index: 5
+  }, {
+    char: "y",
+    index: 4
+  }, {
+    char: "r",
+    index: 13
+  }, {
+    char: "H",
+    index: 0
+  },
+  {
+    char: "e",
+    index: 11
+  }, {
+    char: "a",
+    index: 1
+  }, {
+    char: " ",
+    index: 9
+  }, {
+    char: "!",
+    index: 14
+  }, {
+    char: "e",
+    index: 7
+  }
+];
+
+
+function getStringFromArr(arr) {
+  if (!Array.isArray(arr)) {
+    return;
+  }
+
+  return arr.slice().sort((prev, next) => prev.index - next.index).reduce((acc, current) => (acc += current.char), '');
+}
+
+getStringFromArr(stringArr);
+
+
+const products = [{
+    title: 'prod1',
+    price: 5.2
+  }, {
+    title: 'prod2',
+    price: 0.18
+  },
+  {
+    title: 'prod3',
+    price: 15
+  }, {
+    title: 'prod4',
+    price: 25
+  },
+  {
+    title: 'prod5',
+    price: 18.9
+  }, {
+    title: 'prod6',
+    price: 8
+  },
+  {
+    title: 'prod7',
+    price: 19
+  }, {
+    title: 'prod8',
+    price: 63
+  }
+];
+
+function filterCollection(arr, min, max) {
+
+  return arr.slice().sort((prev, next) => prev.price - next.price).filter((product) => product.price >= min && product.price <= max);
+
+}
+
+filterCollection(products, 15, 30);
+
+// Замыкания
+
+function minus(num1 = 0) {
+  return function (num2 = 0) {
+    return num1 - num2;
+  };
+}
+
+function multiplyMaker(num1) {
+  return function (num2) {
+    return num1 += num2;
+  }
+}
+
+const multiply = multiplyMaker(2);
+
+const modul = (function () {
+  let str = '';
+
+  function setString(val = '') {
+    str = String(val);
+    return str;
+  }
+
+  function getString() {
+    return str;
+  }
+
+  function getStringlength() {
+    return str.length;
+  }
+
+  function reverseString() {
+    return str.split('').reverse().join('');
+  }
+
+  return {
+    setString,
+    getString,
+    getStringlength,
+    reverseString,
+  };
+
+})();
